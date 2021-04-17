4692904918eb8ca164542c3ba3e4321957b8b5b1
Eluan19 <80792077+Eluan19@users.noreply.github.com>
2021-04-16 21:36:41 -0300

    Add files via upload
    primeiro codigo da minha vida rs

---
+++ 8/main.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <stdlib.h>
+#include <locale.h>
+#include <cmath>
+
+using namespace std;//É UM SOBRENOME PARA TIPOS, FUNÇÕES E ETC. É UMA FORMA DE EVITAR NOMES IGUAIS FAZENDO TRABALHOS DIFERENTES COLIDIREM
+
+int ConverteBiDec(long long);
+long long converteDecBi(int);
+
+int main(){
+  setlocale(LC_ALL, "Portuguese");//USADO PARA ACENTUAR PALAVRAS EM PORTUGUÊS
+  long long n;
+  int inicio = 1;
+  while(inicio=1)//PERMITE QUE UM CERTO TRECHO DE PROGRAMA SEJA EXECUTADO ENQUANTO UMA CERTA CONDIÇÃO FOR VERDADEIRA
+{
+                              //MENU DA CALCULADORA
+           cout << "*BEM VINDO A CALCULADORA DE BASES**" << endl;
+           cout << "-----------------------------------" << endl;
+           cout << "-----------------------------------\n" << endl;
+           cout << "ESCOLHA UMA DAS OPÇÕES" << endl;
+           cout << "1- Calcular decimal para binário\n2- Calcular binário para decimal" << endl;
+           cout << "3- Para encerrar" << endl;
+  int menu;
+
+          cin >> menu;
+
+  system("clear||cls");//SERVE PARA LIMPAR O CONSOLE A CADA COMANDO
+  if(menu == 1){
+
+
+               //CONVERSÃO DE DECIMAL/BINÁRIO
+
+    long long decimal, binario[20], i=0;
+    cout << "Digite o numero em Decimal:" <<endl;
+    cin >> decimal;
+    while(decimal!=0)
+    {
+        binario[i] = decimal%2;
+        i++;
+        decimal = decimal / 2;
+    }
+    cout << "\nEm binário é: ";
+    for(i=(i-1); i>=0; i--)
+    cout << binario[i];
+    cout << endl;
+
+    system("pause");//PAUSA O CONSOLE DEPOIS DO COMANDO SOLICITADO
+    system("clear||cls");
+
+
+  }else{
+  if(menu == 2){
+
+                   //CONVERSÃO DE BINÁRIO/DECIMAL
+
+        cout << "Calculando binário para decimal!" << endl;
+        cout << "Digite o número em Binário:" << endl;
+        cin >> n;
+        cout << "\nO valor " << n; cout << " em decimal é: " << ConverteBiDec(n) << endl;
+
+  system("pause");
+  system("clear||cls");//LIMPAR O CONSOLE A CADA COMANDO
+
+}else{
+    if(menu == 3){
+
+
+    cout << "Obrigado por usar nossa calculadora" << endl;
+    cin >> menu;
+    }
+}
+}
+
+}
+  return 0;
+}
+
+
+ConverteBiDec(long long n){
+  int Dec = 0, i = 0, sobra;
+  while (n!=0){
+        sobra = n%10;
+        n /= 10;
+        Dec += sobra*pow(2,i);
+        ++i;
+}
+
+    return Dec;
+}
